4ad3fd41d7b580e3820a3acef4a90c00d41441c1
drestew <drestew48@gmail.com>
2022-03-28 21:11:16 -0400

    Add functionality to declare a winner for each round

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const computerPlay = function() {
-    let playChoice = ['rock', 'paper', 'scissors']
-    const selection = playChoice[Math.floor(Math.random() * 3)]
-    console.log(selection)
+    const playChoice = ['rock', 'paper', 'scissors']
+    const playChoiceIndex = Math.floor(Math.random() * 3)
+    const selection = playChoice[playChoiceIndex]
 }
 
-computerPlay()
+
+const playRound = function(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'player wins'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'player wins'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'player wins'
+    } else {
+        return 'computer wins'
+    }
+}
