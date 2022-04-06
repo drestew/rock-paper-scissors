199a850762168a05753f8b7bafbebfa5fc4e7b4e
drestew <drestew48@gmail.com>
2022-04-06 17:47:20 -0400

    Add 5 rounds to game and declare winner

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const computerPlay = function() {
     const playChoice = ['rock', 'paper', 'scissors']
     const playChoiceIndex = Math.floor(Math.random() * 3)
-    const selection = playChoice[playChoiceIndex]
+    return playChoice[playChoiceIndex]
 }
 
+const playerPlay = function() {
+    let playerChoice = prompt('rock, paper, or scissors?')
+    return playerChoice.toLowerCase()
+}
 
 const playRound = function(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'player wins'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
@@ -16,3 +21,18 @@ const playRound = function(playerSelection, computerSelection) {
         return 'computer wins'
     }
 }
+
+const game = function() {
+    let playerScore = 0
+    let computerScore = 0
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(playerPlay, computerPlay)
+        result = 'player wins' ? playerScore++ : computerScore++
+        i++
+    }
+    const winner = playerScore > computerScore ? 'player wins' : 'computer wins'
+    document.querySelector('.winner').textContent = winner 
+}
+
+game()
+
