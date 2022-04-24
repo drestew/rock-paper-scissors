dd82ce051101e72a7ad0b0f11339527d0402f242
drestew <drestew48@gmail.com>
2022-04-24 00:53:13 -0400

    Add winner of first to 5 points and refactor score tally

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const playerOptions = {
+  paper: { beats: 'rock', loses: 'scissors' },
+  rock: { beats: 'scissors', loses: 'paper' },
+  scissors: { beats: 'paper', loses: 'rock' },
+}
+
 const computerPlay = function () {
   const playChoice = ['rock', 'paper', 'scissors']
   const playChoiceIndex = Math.floor(Math.random() * 3)
@@ -11,32 +17,51 @@ const userSelection = userOptions.forEach((selection) => {
   })
 })
 
+// Playing the game
+const selectWinner = function (roundWinner) {
+  let winner
+  if (roundWinner === 'tie') {
+    winner = 'Tie!'
+  } else {
+    winner = roundWinner === 'user' ? 'You Win!' : 'Computer Wins!'
+  }
+  document.querySelector('.winner').textContent = winner
+}
+
 let userScore = 0
 let computerScore = 0
-const pointScore = function (roundWinner) {
-  roundWinner === 'user' ? (userScore += 1) : (computerScore += 1)
+const scoreTally = function (roundWinner) {
+  let curScore
+  if (roundWinner === 'user') {
+    userScore += 1
+    curScore = userScore
+  } else if (roundWinner === 'computer') {
+    computerScore += 1
+    curScore = computerScore
+  }
+  const scoreEl = document.querySelector(`.players__${roundWinner}-score`)
+  scoreEl.textContent = curScore
+  if (userScore === 5 || computerScore === 5) {
+    document.querySelector('.winner').textContent = `${
+      roundWinner === 'user' ? 'You win' : 'Computer wins'
+    } the game!`
+  } else {
+    selectWinner(roundWinner)
+  }
+}
+
+const currentScore = function (roundWinner) {
+  const winner = roundWinner === 'user' ? userScore : computerScore
+  document.querySelector(`.players__${roundWinner}-score`).textContent = winner
 }
 
 const playRound = function (userSelection, computerSelection) {
   userSelection = userSelection.toLowerCase()
-  if (userSelection === 'rock' && computerSelection === 'scissors') {
-    pointScore('user')
-    document.querySelector('.players__user-score').textContent = userScore
-    document.querySelector('.winner').textContent = 'You Win!'
-  } else if (userSelection === 'scissors' && computerSelection === 'paper') {
-    pointScore('user')
-    document.querySelector('.players__user-score').textContent = userScore
-    document.querySelector('.winner').textContent = 'You Win!'
-  } else if (userSelection === 'paper' && computerSelection === 'rock') {
-    pointScore('user')
-    document.querySelector('.players__user-score').textContent = userScore
-    document.querySelector('.winner').textContent = 'You Win!'
-  } else if (userSelection === computerSelection) {
-    document.querySelector('.winner').textContent = 'Tie!'
+  if (playerOptions[userSelection].beats === computerSelection) {
+    scoreTally('user')
+  } else if (playerOptions[userSelection] === computerSelection) {
+    scoreTally('tie')
   } else {
-    pointScore('computer')
-    document.querySelector('.players__computer-score').textContent =
-      computerScore
-    document.querySelector('.winner').textContent = 'Computer Wins!'
+    scoreTally('computer')
   }
 }
