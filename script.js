3414b093779de5aa19fb0b570f24e98448d82a8b
drestew <drestew48@gmail.com>
2022-04-23 23:18:30 -0400

    Add user choice from button selection

--- script.js
+++ script.js
@@ -4,51 +4,39 @@ const computerPlay = function () {
   return playChoice[playChoiceIndex]
 }
 
-const playerPlay = function () {
-  let playerChoice = prompt('rock, paper, or scissors?')
-  return playerChoice.toLowerCase()
+const userOptions = document.querySelectorAll('.user-btn')
+const userSelection = userOptions.forEach((selection) => {
+  selection.addEventListener('click', function () {
+    playRound(this.textContent, computerPlay())
+  })
+})
+
+let userScore = 0
+let computerScore = 0
+const pointScore = function (roundWinner) {
+  roundWinner === 'user' ? (userScore += 1) : (computerScore += 1)
 }
 
-const playRound = function (playerSelection, computerSelection) {
-  //playerSelection.toLowerCase()
-  if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return 'player wins'
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'player wins'
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return 'player wins'
-  } else if (playerSelection === computerSelection) {
-    return 'tie'
+const playRound = function (userSelection, computerSelection) {
+  userSelection = userSelection.toLowerCase()
+  if (userSelection === 'rock' && computerSelection === 'scissors') {
+    pointScore('user')
+    document.querySelector('.players__user-score').textContent = userScore
+    document.querySelector('.winner').textContent = 'You Win!'
+  } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+    pointScore('user')
+    document.querySelector('.players__user-score').textContent = userScore
+    document.querySelector('.winner').textContent = 'You Win!'
+  } else if (userSelection === 'paper' && computerSelection === 'rock') {
+    pointScore('user')
+    document.querySelector('.players__user-score').textContent = userScore
+    document.querySelector('.winner').textContent = 'You Win!'
+  } else if (userSelection === computerSelection) {
+    document.querySelector('.winner').textContent = 'Tie!'
   } else {
-    return 'computer wins'
-  }
-}
-
-const game = function () {
-  let playerScore = 0
-  let computerScore = 0
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = playerPlay()
-    let computerChoice = computerPlay()
-    let result = playRound(playerChoice, computerChoice)
-    if (result === 'player wins') {
-      playerScore++
-    } else if (result === 'computer wins') {
-      computerScore++
-    }
-  }
-  const winner = playerScore > computerScore ? 'PLAYER!' : 'COMPUTER!'
-  console.log('winner is ', winner, playerScore, computerScore)
-  document.querySelector('.winner').textContent = winner
-  const scoreDiv = document.createElement('div')
-  const scoreText = document.createTextNode(
-    `Final score is ${Math.max(playerScore, computerScore)} to ${Math.min(
-      playerScore,
+    pointScore('computer')
+    document.querySelector('.players__computer-score').textContent =
       computerScore
-    )}`
-  )
-  scoreDiv.appendChild(scoreText)
-  document.body.insertBefore(scoreText, scoreDiv.nextSibling)
+    document.querySelector('.winner').textContent = 'Computer Wins!'
+  }
 }
-
-game()
